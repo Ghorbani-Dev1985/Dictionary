acc0b3cad2d37693480425adb4bec099d2d3d6c2
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-20 15:49:48 +0330

    fix: handle undefied data from api with ternary operator

--- JS/Main.js
+++ JS/Main.js
@@ -1,65 +1,74 @@
 const $ = document;
-const DicWordInput = $.querySelector("#DicWordInput")
-const DicResultContainer = $.querySelector("#DicResultContainer")
-const DicDefinitions = $.querySelector("#DicDefinitions")
+const DicWordInput = $.querySelector("#DicWordInput");
+const DicContainerDetails = $.querySelector("#DicContainerDetails");
 
-function GetDicData(){
-    let getUserWord = DicWordInput.value;
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${getUserWord}`)
+function GetDicData() {
+  let getUserWord = DicWordInput.value;
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${getUserWord}`)
     .then((res) => res.json())
     .then((data) => {
-        ShowWordTranslate(data);
+      ShowWordTranslate(data);
+    })
+    .catch(err =>{
+        ShowError();
+        console.log(err);
     })
 }
 
-
-
-function ShowWordTranslate(data){
-    // console.log(data[1].meanings[0].partOfSpeech);
-    DicResultContainer.innerHTML = "";
-    let WordNum = 0;
-    data.forEach(dicData => {
+function ShowWordTranslate(data) {
+  DicWordInput.value = "";
+  DicContainerDetails.classList.remove('error')
+  DicContainerDetails.innerHTML = "";
+  let WordNum = 0;
+      data.forEach((dicData) => {
         console.log(dicData);
-        WordNum ++;
-        DicResultContainer.insertAdjacentHTML('beforeend' , `
-        <p class="DicWord">${WordNum}- ${dicData.word}</p>
-        <p class="DicPartOfSpeech">${dicData.meanings[0].partOfSpeech}</p>
-        <div class="flex-center">
-        UK
-        <audio id="UkAudio" src=${dicData.phonetics[0].audio}></audio>
-        <div onClick="PlayUkAudio()"><img src="./../Images/audio.svg" alt="ghorbani-dev.ir"/></div>
-        ${dicData.phonetics[0].text}
-        US
-        <audio id="UsAudio" src=${dicData.phonetics[1].audio}></audio>
-        <div onClick="PlayUsAudio()"><img src="./../Images/audio.svg" alt="ghorbani-dev.ir"/></div>
-        ${dicData.phonetics[1].text}
-        </div>
-        `)
-    });
-}
+        WordNum++;
+        DicContainerDetails.insertAdjacentHTML(
+          "beforeend",
+          `
+            <p class="DicWord">${WordNum}- ${dicData.word}</p>
+            <p class="DicPartOfSpeech">${dicData.meanings[0].partOfSpeech}</p>
+            <div class="flex-start">
+            ${dicData.phonetics !== undefined ? `
+             UK
+            <audio id="UkAudio" src=${dicData.phonetics[0].audio}></audio>
+            <div onClick="PlayUkAudio()"><img src="./../Images/audio.svg" alt="ghorbani-dev.ir"/></div>
+            ${dicData.phonetics[0].text}
+            ` : ""}
+           
+            ${dicData.phonetics[1] !== undefined ? `  US
+            <audio id="UsAudio" src=${dicData.phonetics[1].audio}></audio>
+            <div onClick="PlayUsAudio()"><img src="./../Images/audio.svg" alt="ghorbani-dev.ir"/></div>${dicData.phonetics[1].text}` : ""}
+            </div>
+            <p class="DicDefinition">${dicData.meanings[0].definitions[0].definition}</p>
+            ${dicData.meanings[0].definitions[0].example !== undefined ? `
+              <ul><li>${ dicData.meanings[0].definitions[0].example }</li></ul>` : ""
+            }
+          
+            `
+        );
+      });
 
-// ${dicData.meanings[0].definitions.forEach(meaning => {
-//     `${meaning.definition}
-//     ${meaning.example}`
-     
-     
-//      console.log(meaning);
-//   })}
-
-function PlayUkAudio(){
-    let UkAudio = $.querySelector('#UkAudio');
-    UkAudio.play();
-}
-
-function PlayUsAudio(){
-    let UsAudio = $.querySelector('#UsAudio');
-    UsAudio.play();
 }
 
 
+function ShowError(){
+    DicContainerDetails.classList.add('error')
+    DicContainerDetails.innerHTML = "Sorry! Your word not found ...."
+}
 
-DicWordInput.addEventListener("keypress" , (e) => {
-    if(e.keyCode === 13){
-        GetDicData();
-    }
-})
+function PlayUkAudio() {
+  let UkAudio = $.querySelector("#UkAudio");
+  UkAudio.play();
+}
+
+function PlayUsAudio() {
+  let UsAudio = $.querySelector("#UsAudio");
+  UsAudio.play();
+}
+
+DicWordInput.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    GetDicData();
+  }
+});
