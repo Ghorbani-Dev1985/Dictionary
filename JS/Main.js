373b6c0f4fbbf03993418c76a86a0b9e898a5ba4
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-20 12:51:13 +0330

    feat: fetch data from api and show in log

--- JS/Main.js
+++ JS/Main.js
@@ -1,2 +1,30 @@
 const DicWordInput = document.querySelector("#DicWordInput")
 const DicResultContainer = document.querySelector("#DicResultContainer")
+
+
+function GetDicData(){
+    let getUserWord = DicWordInput.value;
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${getUserWord}`)
+    .then((res) => res.json())
+    .then((data) => {
+        ShowWordTranslate();
+        console.log(data);
+    })
+}
+
+
+
+function ShowWordTranslate(){
+
+}
+
+
+
+
+
+
+DicWordInput.addEventListener("keypress" , (e) => {
+    if(e.keyCode === 13){
+        GetDicData();
+    }
+})
